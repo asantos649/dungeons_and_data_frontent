de50388cc19123969e09b93e150008616c582149
brewchetta <chett.tiller@gmail.com>
2020-03-05 14:48:11 -0500

    Converted RightSideForm to hooks, added notes for future content

--- src/containers/RightSideForm.js
+++ src/containers/RightSideForm.js
@@ -1,37 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CoreStat from '../components/CoreStat'
 import CharStat from '../components/CharStat'
 import ListBox from '../components/ListBox'
 
-class RightSideForm extends React.Component{
+const RightSideForm = props => {
 
-    render(){
-        return(
-            <div className = 'rightSideForm'>
-                <div className='coreStats'>
-                    <CoreStat stat='STR'/>
-                    <CoreStat stat='CON'/>
-                    <CoreStat stat='DEX'/>
-                    <CoreStat stat='WIS'/>
-                    <CoreStat stat='INT'/>
-                    <CoreStat stat='CHA'/>
-                </div>
-                <div className='charStats'>
-                    <CharStat stat='AC'/>
-                    <CharStat stat='HP'/>
-                    <CharStat stat='Speed'/>
-                </div>
-                <div className='topList'>
-                    <ListBox title='Equipment'/>
-                    <ListBox title='Attacks'/>
-                </div>
-                <div className='bottomList'>
-                    <ListBox title='Abilities/Spells'/>
-                    <ListBox title='Proficiencies/Languages'/>
-                </div>
-            </div>
-        )
-    }
+  const [character, setCharacter] = useState({
+    name: '',
+    str: 0,
+    con: 0,
+    dex: 0,
+    int: 0,
+    wis: 0,
+    cha: 0,
+    lawfulChaotic: 'lawful',
+    goodEvil: 'good',
+    class: '',
+    race: '',
+    equipment: '',
+    attacks: '',
+    abilitiesSpells: '',
+    proficiencies: [],
+    status: 'alive',
+    flaws: ''
+  })
+
+  return(
+    <div className = 'rightSideForm'>
+
+        <div className='bioStats'>
+          // name goes here
+        </div>
+
+        <div className='coreStats'>
+            <CoreStat stat='STR'/>
+            <CoreStat stat='CON'/>
+            <CoreStat stat='DEX'/>
+            <CoreStat stat='WIS'/>
+            <CoreStat stat='INT'/>
+            <CoreStat stat='CHA'/>
+        </div>
+
+        <div className='alignmentStats'>
+          // alignment goes here
+        </div>
+
+        <div className='classStat'>
+          // Class goes here
+        </div>
+
+        <div className='raceStat'>
+          // race goes here
+        </div>
+
+        <div className='topList'>
+            <ListBox title='Equipment'/>
+            // Text
+            <ListBox title='Attacks'/>
+        </div>
+
+        <div className='bottomList'>
+            <ListBox title='Abilities/Spells'/>
+            // Text
+            <ListBox title='Proficiencies'/>
+            // List
+        </div>
+
+        <div className='status'>
+          // Character status goes here (alive, dead, retired, etc)
+        </div>
+
+        <div className='flawStats'>
+          // flaw goes here
+          // Text
+        </div>
+
+    </div>
+  )
 }
 
 export default RightSideForm
